51a1c966c4b785b5702dc636cf63f27cac565f89
Wayne Barber <68376947+wbarber93@users.noreply.github.com>
2021-02-10 14:50:13 +0000

    Update data_cleaner.py

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from datetime import datetime
+import numpy as np
 
 path = os.getcwd()
 print(path)
@@ -14,30 +14,38 @@ df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit ='ms')
 date_only = ((df['Timestamp']).dt.date)
 df['Date'] = date_only
 df = df.set_index(['Timestamp'])
-df = (df.loc['2010-09-07 00:00:01' : '2010-12-16 23:59:59'])
-#print(df)
+df = (df.loc['2010-09-07 00:00:01' : '2010-12-06 23:59:59'])
 
-# Creates second dataframe to drop duplicates and header each day
+# Creates date headers and first price traded each day
 df2 = df.drop_duplicates(subset=['Date'])
-#df2.reset_index(drop=True, inplace=True)
-#df2.set_index(['Date'])
-print(df2)
 
-
-df2.between_time('17:15:00' , '23:59:59')
-
-
+# Calculates the VMAP between 1714 and 1715
 df3 = df.between_time('17:14:00' , '17:14:59')
-df3.loc['Price*Vol'] = ((df3["Price"] * df3['Volume']))
+df3['PriceVol'] = (df3.Price * df3.Volume)
 df3 = df3.groupby(['Date']).sum()
+df3['VWAP'] = (df3.PriceVol / df3.Volume)
 
-df3['VWAP'] = (df3['Price*Vol'] / df3['Volume'])
-print(df3)
+# Flags and seperates trades that occur after 1715 with identical price to first traded price
+df4 = df.between_time('17:15:00' , '23:59:59')
+df_combined = pd.merge(df2, df4, on=['Date'])
+df_combined['Flag'] = np.where(df_combined.Price_x == df_combined.Price_y, 'True', 'False')
+df_boolean = df_combined[df_combined['Flag'] == 'True']
+df_boolean = df_boolean.drop_duplicates(subset=['Date'])
 
+# Cleans up and merges dataframes so requested data remains
+df3 = df3.drop(['Price', 'Volume', 'PriceVol'], axis=1)
+df2 = df2.drop(['Ref Timestamp', 'Volume'], axis=1)
+df_results = pd.merge(df3, df2, on=['Date'])
+df_boolean = df_boolean[df_boolean.columns.difference(['Price_x', 'Volume_x', 'Ref Timestamp_x', 'Volume_y', 'Price_y'])]
+df_results = df_results.merge(df_boolean, how = 'left', left_on=['Date'], right_on=['Date'])
 
+# Cleans up final dataframe with all requested data
+df_results = df_results.rename(columns={"Price": "First Trading Price", "Ref Timestamp_y": "Time"})
+df_results = df_results[['Date', 'First Trading Price' , 'VWAP', 'Flag', 'Time']]
+df_results = df_results.replace(np.nan, '', regex=True)
+df_results['Time'] = (pd.to_datetime(df_results['Time'])).dt.time
+df_results['Time'] = df_results['Time'].fillna('')
 
-#df3['Traded after 1715'] = 
+print(df_results)
 
-
-
-
+df_results.to_csv('Results.csv', index=False)
